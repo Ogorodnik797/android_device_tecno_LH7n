56cd5c8c78599b44ad78f3b0dc57e528f7e363a9
Ogorodnik797 <oliaselezneva60@outlook.com>
2026-01-06 14:24:25 +0700

    LH7n:  Miku

--- miku_LH7n.mk
+++ miku_LH7n.mk
@@ -10,11 +10,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/tecno/LH7n/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common miku stuff.
+$(call inherit-product, vendor/miku/config/common_full_phone.mk)
 
 BOARD_VENDOR := TECNO
-PRODUCT_NAME := lineage_LH7n
+PRODUCT_NAME := miku_LH7n
 PRODUCT_DEVICE := LH7n
 PRODUCT_MANUFACTURER := TECNO
 PRODUCT_BRAND := TECNO
@@ -28,3 +28,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Time
 LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
+
+# Miku flags
+MIKU_GAPPS := true
+MIKU_MASTER := CRAZY_JOKER67
