62e7705526a8f6174210ab26ccdd6a2a555f5ff1
Ogorodnik797 <oliaselezneva60@outlook.com>
2026-01-25 09:53:44 +0300

    Halcyon bringup

--- halcyon_LH7n.mk
+++ halcyon_LH7n.mk
@@ -10,8 +10,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/tecno/LH7n/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Halcyon stuff.
+$(call inherit-product, vendor/halcyon/config/common_full_phone.mk)
 
 BOARD_VENDOR := TECNO
 PRODUCT_NAME := lineage_LH7n
